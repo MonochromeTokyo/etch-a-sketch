18a1ca23def2d8ddeed359c9f00837072774704c
MonochromeTokyo <122960478+MonochromeTokyo@users.noreply.github.com>
2023-06-04 17:09:54 -0400

    Add an Experimental 16x16 grid.

---
+++ sketch.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('.grid-container');
+// Create 16 x 16 Grid
+for( let i = 0; i < 16; i++){
+    for(let j = 0; j < 16; j++) {
+        const div = document.createElement('div');
+        div.classList.add('grid-item');
+        container.appendChild(div);
+
+    }
+}

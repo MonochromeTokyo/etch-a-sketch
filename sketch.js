9fa2002fa014f97ec180052159a1888e52dbd232
MonochromeTokyo <122960478+MonochromeTokyo@users.noreply.github.com>
2023-06-06 23:06:50 -0400

    Get 16x16 grid to display properly

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.grid-container');
 // Create 16 x 16 Grid
 for( let i = 0; i < 16; i++){
+    console.log(i);
     for(let j = 0; j < 16; j++) {
         const div = document.createElement('div');
         div.classList.add('grid-item');

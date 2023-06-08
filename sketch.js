b3b98668a8de4588335722865f820009167aece7
MonochromeTokyo <122960478+MonochromeTokyo@users.noreply.github.com>
2023-06-07 22:47:09 -0400

    Create working grids with color changing Highlighting over each div will turn it's color black.

--- sketch.js
+++ sketch.js
@@ -1,11 +1,23 @@
 const container = document.querySelector('.grid-container');
-// Create 16 x 16 Grid
+
+//Function to change the color of an element
+function changeColor(){
+    this.style.backgroundColor = 'black';
+
+}
+
+
+
+// Create 16 x 16 Grid and assign an event listener to each div
 for( let i = 0; i < 16; i++){
     console.log(i);
     for(let j = 0; j < 16; j++) {
         const div = document.createElement('div');
         div.classList.add('grid-item');
         container.appendChild(div);
+        div.addEventListener('mouseover', changeColor);
 
     }
 }
+
+
